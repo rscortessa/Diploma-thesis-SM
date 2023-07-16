0d6ac2fc5ed1d4ce68cd858c048048edcdcb14fe
Ronalito <rscortessa@unal.edu.co>
2023-07-16 15:14:14 +0200

    changes added

--- graph4/read4.py
+++ graph4/read4.py
@@ -25,37 +25,58 @@ L=int(arg[5])
 t=int(arg[6])
 howmuch=int(arg[7])
 portion=int(arg[8])
-m=dp/zas*N
-num=int(m/N)
-###Length of the file: m*N
+
 read_portion=int(m*portion/100)
 r_portion=(portion/100*N)
 
+entire_set=True
+
+if portion==100:
+    entire_set=True
+else:
+    entire_set=False
+
+m=dp/zas*N
+num=int(m/N)
+
+###Length of the file: m*N
+
 filename="./graph4/DP_L"+str(L)+"T"+str(t)+"P("+str(pp)+"-"+str(pp+dp)+")S"+str(L)+".txt"
+
+
+
 
 
 A=[[0 for i in range(num)],[0 for i in range(num)]]
 pca_L=np.array([])
 pca=IncrementalPCA(n_components=1)
 
-tracemalloc.start()
+if entire_set==True:
+    files=pd.read_csv(filename,delim_whitespace=True,header=None,dtype=np.uint8)
+    scaler=StandardScaler()
+    scaler.fit(files)
+    scaled_data=scaler.transform(files)
+    pca=PCA(n_components=1)
+    pca.fit(scaled_data)
+    x_pca=pca.transform(scaled_data)
 
-with pd.read_csv(filename,delim_whitespace=True,header=None,dtype=np.uint8,chunksize=read_portion) as reader:
-    for chunk in reader:
-        print(chunk,sys.getsizeof(chunk))
-        pca.partial_fit(chunk)
-        
-print("PCA",sys.getsizeof(pca))
+else:
+    tracemalloc.start()
+    with pd.read_csv(filename,delim_whitespace=True,header=None,dtype=np.uint8,chunksize=read_portion) as reader:
+        for chunk in reader:
+            print(chunk,sys.getsizeof(chunk))
+            pca.partial_fit(chunk)    
+    print("PCA",sys.getsizeof(pca))
 
-with pd.read_csv(filename,delim_whitespace=True,header=None,dtype=np.uint8,chunksize=read_portion) as reader:
-    for chunk in reader:
-        x_pca=pca.transform(chunk)
-        x_pca=np.abs(x_pca[:,0])
-        pca_L=np.concatenate((pca_L,x_pca),axis=0)
+    with pd.read_csv(filename,delim_whitespace=True,header=None,dtype=np.uint8,chunksize=read_portion) as reader:
+        for chunk in reader:
+            x_pca=pca.transform(chunk)
+            x_pca=np.abs(x_pca[:,0])
+            pca_L=np.concatenate((pca_L,x_pca),axis=0)
+    print(tracemalloc.get_traced_memory())
+    tracemalloc.stop()
 
-print(tracemalloc.get_traced_memory())
-tracemalloc.stop()
-
+    
 for j in range(num):
         A[0][j]=np.mean(pca_L[N*j:N*(j+1)])
         A[1][j]=np.std(pca_L[N*j:N*(j+1)])/np.sqrt(N*1.0)
